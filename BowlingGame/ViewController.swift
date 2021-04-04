00ba6d3bb238645cc2fb97fb58b339a1461d349a
Ilhansari <ilhansari94@hotmail.com>
2021-04-04 14:06:04 +0300

    First commit Bowling game TDD

---
+++ BowlingGame/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BowlingGame
+//
+//  Created by Ilhan Sari on 4.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ BowlingGameTests/BowlingGameTests.swift
@@ -0,0 +1,24 @@
+//
+//  BowlingGameTests.swift
+//  BowlingGameTests
+//
+//  Created by Ilhan Sari on 4.04.2021.
+//
+
+import XCTest
+@testable import BowlingGame
+
+final class Game {
+
+    func roll(_ pins: Int) {
+
+    }
+
+    func score() -> Int {
+        return 1
+    }
+}
+
+class BowlingGameTests: XCTestCase {
+
+}
