d0af8c1669abc490cccd876a8e547db71f9921fe
Ilhansari <ilhansari94@hotmail.com>
2021-04-05 07:12:53 +0300

    Create gutter game and score logic

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -5,20 +5,47 @@
 //  Created by Ilhan Sari on 4.04.2021.
 //
 
-import XCTest
 @testable import BowlingGame
+import XCTest
 
 final class Game {
 
-    func roll(_ pins: Int) {
+    private var theScore = 0
 
+    func roll(_ pins: Int) {
+        theScore += pins
     }
 
     func score() -> Int {
-        return 1
+        return theScore
     }
 }
 
-class BowlingGameTests: XCTestCase {
+final class BowlingGameTests: XCTestCase {
 
+    private var game: Game!
+
+    override func setUp() {
+        super.setUp()
+        game = Game()
+    }
+
+    override func tearDown() {
+        game = nil
+        super.tearDown()
+    }
+
+    func testGutterGame() {
+        for _ in 1...20 {
+            game.roll(0)
+        }
+        XCTAssertEqual(game.score(), 0)
+    }
+
+    func testAllOnes() {
+        for _ in 1...20 {
+            game.roll(1)
+        }
+        XCTAssertEqual(game.score(), 20)
+    }
 }
