59204697e19ab7dd62c721c8d0209279ab4c5404
Ilhansari <ilhansari94@hotmail.com>
2021-04-07 08:12:26 +0300

    Refactor score function

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -20,17 +20,21 @@ final class Game {
 
     func score() -> Int {
         var score = 0
-        var i = 0
+        var roll = 0
         for _ in 1...10 {
-            if rolls[i] + rolls[i+1] == 10 {
-                score += 10 + rolls[i+2]
-                i += 2
+            if isSpare(roll) {
+                score += 10 + rolls[roll + 2]
+                roll += 2
             } else {
-                score += rolls[i] + rolls[i+1]
-                i += 2
+                score += rolls[roll] + rolls[roll + 1]
+                roll += 2
             }
         }
         return score
+    }
+
+    private func isSpare(_ roll: Int) -> Bool {
+        return rolls[roll] + rolls[roll + 1] == 10
     }
 }
 
